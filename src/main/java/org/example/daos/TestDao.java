5f03f06162189afcd367c4e808e2a29918e1599b
Ashna Abraham <ashna.abraham@kainos.com>
2024-07-03 23:09:32 +0100

    merge conflict

--- src/main/java/org/example/daos/TestDao.java
+++ src/main/java/org/example/daos/TestDao.java
@@ -15,18 +15,18 @@ public class TestDao {
             Statement statement = connection.createStatement();
 
             long start = System.currentTimeMillis();
-
             ResultSet resultSet = statement.executeQuery(
                     "SHOW DATABASES;");
+            long end = System.currentTimeMillis();
 
-           long end = System.currentTimeMillis();
+            System.out.println("Total time " + (end - start));
 
-
-           System.out.println("total time" + (end - start));
-
-           while (resultSet.next()) {
+            System.out.println("Retrieving database names");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("Database"));
                 databases.add(resultSet.getString("Database"));
             }
+            System.out.println("Finished retrieving database names");
         }
 
         return databases;
